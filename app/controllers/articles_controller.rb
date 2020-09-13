1f2f0ce6cae4851788825ec40cfcdd5f2190fd99
Mahmoud Bakr <mbakr6821@gmail.com>
2020-09-13 19:21:20 +0200

    fix update action in articles_controller.rb file

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -26,8 +26,9 @@ class ArticlesController < ApplicationController
     end
 
     def update
-        @article = Article.find(article_params)
+        @article = Article.find(params[:id])
         @article.update(article_params)
+
         redirect_to article_path(@article)
     end
 
